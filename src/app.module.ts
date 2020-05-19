f38e3a10e0ec432ab3dec94e7d90a88b7b8842cb
Antoine <karagan.app@gmail.com>
2020-05-19 14:48:41 +0200

    setup config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,35 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 import { config } from './config/config';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
+    ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(config as TypeOrmModuleOptions),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1435,
+      username: 'sa',
+      password: 'reallyStrongPwd123',
+      database: 'back_admin',
+      synchronize: true,
+      requestTimeout: 3000,
+      // host: process.env.DATABASE_URL,
+      // port: +process.env.DATABASE_PORT,
+      // username: process.env.DATABASE_USERNAME,
+      // password: process.env.DATABASE_PASSWORD,
+      // database: process.env.DATABASE_NAME,
+      // synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+      // requestTimeout: +process.env.DATABASE_REQUEST_TIMEOUT,
+      logging: true,
+      entities: [`dist/**/*.entity{.ts,.js}`],
+    }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
-export class AppModule {}
+export class AppModule { }
